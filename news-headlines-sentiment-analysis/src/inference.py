75bf94c978f20101266b0f771a3547e34b648eff
jonthan kovler <kovler.jonathan@gmail.com>
2024-11-16 22:23:19 +0200

    inference

---
+++ news-headlines-sentiment-analysis/src/inference.py
@@ -0,0 +1,38 @@
+from numpy.ma.setup import configuration
+from transformers import AutoTokenizer, AutoModelForSequenceClassification, pipeline
+import torch
+from configuration import PATH_TO_BASE_MODEL
+from src.sentiment_analysis import sentiment_analysis
+
+# Load the fine-tuned model and tokenizer
+model_path = "./heBERT-news-sentiment-classifier"  # The directory where you saved your model
+tokenizer = AutoTokenizer.from_pretrained(model_path)
+fine_tuned_model = AutoModelForSequenceClassification.from_pretrained(model_path)
+
+base_model = AutoModelForSequenceClassification.from_pretrained(PATH_TO_BASE_MODEL)
+
+heBERT_news_sentiment_classifier = pipeline(
+    "sentiment-analysis",
+    model=fine_tuned_model,
+    tokenizer=tokenizer,
+    return_all_scores=False,
+    device=0
+
+)
+
+heBERT = pipeline(
+    "sentiment-analysis",
+    model=PATH_TO_BASE_MODEL,
+    tokenizer=tokenizer,
+    return_all_scores=False,
+    device=0
+
+)
+# List of sentences to classify
+texts = ["צהל חיסל מחבלים", "ישראל תקפה אווירית", "ישראל ניצחה", "חמאס תקף את ישראל", "אני שמח", "איזה יום גרוע היה לי",
+         "צהל הצליח לשמור על המדינה"]
+
+for text in texts:
+    print(f"base model-->> text{text}, prediction{heBERT(text)}")
+    print(f"fine-tuned model-->> text{text}, prediction{heBERT_news_sentiment_classifier(text)}")
+    print()
